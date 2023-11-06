c63f9af3110340640d7f545320c80d89f5831657
haileemin <dr.leminhhai2003@gmail.com>
2023-11-06 20:58:11 +0700

    git commit -mfirst git commit -m

---
+++ Getting JS/JAVA/script.js
@@ -0,0 +1,21 @@
+let a = 6; 
+let b = 10;
+
+let c = a + b;
+
+console.log("Tổng Là: " + c);
+
+
+//hàm alert
+// alert("Tổng là: " + c);
+
+
+
+// hàm prompt: dùng để nhập dữ liệu
+// prompt("Nhập số: ");
+
+//hàm confirm(): thông báo, xác nhận 1 vấn đề thôi
+// confirm("Bạn có muốn nhập con số này không?");
+
+// in trên trang web: hàm Document.write()
+// document.write("Tổng là: " + c);
